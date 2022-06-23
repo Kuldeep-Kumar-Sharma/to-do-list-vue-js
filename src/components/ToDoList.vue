4a3c6ad0853f9af076495a3f90ad4b4f9deec5a5
Kuldeep Kumar <sharman94kuldeep@gmail.com>
2022-06-23 21:06:29 +0200

    initial commit

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>To Do List</header>
+
+  
+</template>
+
+<script>
+export default {
+  name: 'to-do-list',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  color: white;
+  padding: 20px;
+  background-color: black;
+}
+
+</style>

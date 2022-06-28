5d89286025d0dd9d0a70636153c93a82fbe74792
Kuldeep Kumar <sharman94kuldeep@gmail.com>
2022-06-28 22:41:53 +0200

    adding task

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,36 @@ export default {
     ToDoList
   }
 }
+
+
+
+/**
+const express = require("express");
+
+
+ *
+ * @returns {express.Application}
+ 
+const createExpressApp = () => {
+  const app = express();
+  app.get('*', function (req, res) {
+    let operator = req.url.substring(req.url.indexOf('+'), req.url.indexOf('+') + 1);
+    console.log(1);
+    // let nums = req.url.split(operator);
+    // let num1 = parseInt(nums[0].substring(1));
+    // let num2 = parseInt(nums[0]);
+    // let answer = "VALIDATION_ERROR";
+    // switch(operator){
+    //     case '+':
+    //         answer = num1+num2;
+    //     break;
+    // }
+    // console.log(answer);
+    res.send({ "result": "" });
+  });
+  return app;
+};
+***/
+
 </script>
+
--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,13 +1,85 @@
 <template>
   <header>To Do List</header>
+  <div class="card">
+    <div class="container">
+      <label for="taskName">Enter the task name </label><br />
+      <input type="text" id="taskName" placeholder="Check something" v-model="taskName">
 
-  
+      <button class="addButton" @click="addToList"> + </button>
+
+      <ul>
+        <li v-for="task in taskList" :key="task">
+          {{task}}
+        </li>
+      </ul>
+    </div>
+  </div>
+  {{taskName}}
 </template>
 
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  width: 60%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+
+input[type=text] {
+  width: 80%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 2px solid red;
+}
+
+.addButton {
+  background-color: #4CAF50;
+  /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  background-color: white;
+  color: black;
+  border: 2px solid #4CAF50;
+}
+
+.addButton:hover {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
 <script>
 export default {
   name: 'to-do-list',
+  data() {
+    return {
+      taskList:[],
+      taskName: ''
+    }
+  },
   props: {
+  },
+  methods: {
+    addToList: function() {
+      this.taskList.push(this.taskName);
+      this.taskName = '';
+    }
   }
 }
 </script>
